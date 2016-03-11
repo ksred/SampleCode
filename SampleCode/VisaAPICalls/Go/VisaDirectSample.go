525334c038943aa259f3a808f66ab3cbb09c130a
Kyle Redelinghuys <kyle@ksred.me>
2016-03-11 06:31:37 -0500

    Added visa direct sample
    Visa Direct sample and test added
    (Untested)

---
+++ SampleCode/VisaAPICalls/Go/VisaDirectSample.go
@@ -0,0 +1,186 @@
+package visasample
+
+import (
+	"bytes"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const USER_ID = ""
+const USER_PASSWORD = ""
+
+const SSL_PUBLIC_KEY_PATH = "certs/application.crt"
+const SSL_PRIVATE_KEY_PATH = "certs/application.pem"
+const SSL_CAPRIVATE_KEY_PATH = "certs/VDPCA-SBX.pem"
+
+const API_SANDBOX = "https://sandbox.api.visa.com"
+const API_PORT = 433
+
+var PULL_FUNDS_TRANSACTIONS_URL = API_SANDBOX + "/visadirect/fundstransfer/v1/pullfundstransactions/"
+
+type PullFundsTransactionRequest struct {
+	SystemsTraceAuditNumber       int                       `json:"systemsTraceAuditNumber"`                 // required, 6
+	RetrievalReferenceNumber      string                    `json:"retrievalReferenceNumber"`                // ydddhhnnnnnn(numeric characters only), Length: 12
+	LocalTransactionDateTime      string                    `json:"localTransactionDateTime"`                // RFC3339. dateTime | YYYY-MM-DDThh:mm:ss. The date and time you submit the transaction
+	AcquiringBin                  int                       `json:"acquiringBin"`                            // integer | positive, Length: 6 - 11
+	AcquirerCountryCode           int                       `json:"acquirerCountryCode"`                     // integer | Length: 3
+	SenderPrimaryAccountNumber    string                    `json:"senderPrimaryAccountNumber"`              // string | Length: 13 - 19
+	SenderCardExpiryDate          string                    `json:"senderCardExpiryDate"`                    // string | YYYY-MM
+	SenderCurrencyCode            string                    `json:"senderCurrencyCode"`                      // string | Length: 3
+	Amount                        float64                   `json:"amount,omitempty"`                        // Optional: decimal | Length: totalDigits 12, fractionDigits 3 (minimum value is 0)
+	Surcharge                     float64                   `json:"surcharge,omitempty"`                     // Optional: decimal | Length: totalDigits 12, fractionDigits 3(minimum value is 0)
+	Cavv                          string                    `json:"cavv"`                                    // string | Length:40
+	ForeignExchangeFeeTransaction float64                   `json:"foreignExchangeFeeTransaction,omitempty"` // Optional: decimal | Length: totalDigits 12, fractionDigits 3 (minimum value is 0)
+	BusinessApplicationId         string                    `json:"businessApplicationId"`                   // string | Length: 2
+	MerchantCategoryCode          int                       `json:"merchantCategoryCode,omitempty"`          // Conditional: integer | Length: total 4 digits
+	CardAcceptor                  CardAcceptor              `json:"cardAcceptor"`                            // Object
+	MagneticStripeData            *MagneticStripeData       `json:"magneticStripeData,omitempty"`            // Optional: Object
+	PointOfServiceData            *PointOfServiceData       `json:"pointOfServiceData,omitempty"`            // Conditional: Object
+	PointOfServiceCapability      *PointOfServiceCapability `json:"pointOfServiceCapability,omitempty"`      // Conditional: Object
+	PinData                       *PinData                  `json:"pinData,omitempty"`                       // Conditional: Object
+	FeeProgramIndicator           string                    `json:"feeProgramIndicator,omitempty"`           // Optional: string | Length:3
+}
+
+// --- START Element Structs ---
+type CardAcceptor struct {
+	Name       string              `json:"name"`       // string | Length: 1 - 25
+	TerminalId string              `json:"terminalId"` // string | Length: 1 - 8
+	IdCode     string              `json:"idCode"`     // string | Length: 1 - 15
+	Address    CardAcceptorAddress `json:"address"`    // Object
+}
+
+type CardAcceptorAddress struct {
+	State   string `json:"state,omitempty"`   // Conditional: string | Length: 2
+	County  string `json:"county,omitempty"`  // Conditional: string | Length: 3
+	Country string `json:"country"`           // string | Length: 3
+	ZipCode string `json:"zipCode,omitempty"` // Conditional: string | Length: 5 - 9
+}
+
+type MagneticStripeData struct {
+	Track1Data string `json:"track1Data,omitempty"` // Conditional: string | Length: maximum 76
+	Track2Data string `json:"track2Data,omitempty"` // Conditional: string | hex binary value is sent as String, Length: maximum 19
+}
+
+type PointOfServiceData struct {
+	PanEntryMode     int    `json:"panEntryMode,omitempty"`     // Conditional: integer | positive, Length: totaldigits 2
+	PosConditionCode int    `json:"posConditionCode,omitempty"` // Conditional: integer | positive,Length: totalDigits 2
+	MotoECIIndicator string `json:"motoECIIndicator,omitempty"` // Conditional: string | Length: 1 , max: 1 characters
+}
+
+type PointOfServiceCapability struct {
+	PosTerminalType            int `json:"posTerminalType,omitempty"`            // Conditional: integer | positive, totalDigits 0
+	PosTerminalEntryCapability int `json:"posTerminalEntryCapability,omitempty"` // Conditional: integer | positive, Length: totalDigits 1
+}
+
+type PinData struct {
+	PinDataBlock               string                      `json:"pinDataBlock,omitempty"`               // Conditional: string | Length: 16, hexbinary format
+	SecurityRelatedControlInfo *SecurityRelatedControlInfo `json:"securityRelatedControlInfo,omitempty"` // Conditional: object
+}
+
+type SecurityRelatedControlInfo struct {
+	PinBlockFormatCode int `json:"pinBlockFormatCode,omitempty"` // Conditional: integer |positive Length: totalDigits 2
+	ZoneKeyIndex       int `json:"zoneKeyIndex,omitempty"`       // Conditional: integer |positive Length: totalDigits 2
+}
+
+// --- END Element Structs ---
+
+// --- START Response Structs ---
+type PullFundsTransactionResponse struct {
+	StatusIdentifier      string `json:"statusIdentifier"`              // string | required when call times out
+	TransactionIdentifier int    `json:"transactionIdentifier"`         // integer | positive and required when call does not timeout, Length: 15
+	ActionCode            string `json:"actionCode"`                    // string | Length: 2
+	ApprovalCode          string `json:"ApprovalCode,omitempty"`        // Optional: string | Length: 6
+	TransmissionDateTime  string `json:"transmissionDateTime"`          // dateTime | YYYY-MM-DDThh:mm:ss
+	CavvResultCode        string `json:"cavvResultCode,omitempty"`      // Optional: string | Length: 1
+	ResponseCode          string `json:"responseCode"`                  // string | Length: 1
+	FeeProgramIndicator   string `json:"feeProgramIndicator,omitempty"` // Optional: string | Length:3
+	ErrorMessage          string `json:"errorMessage,omitempty"`        // Optional: string | Length:3
+}
+
+func main() {
+}
+
+// PullFundsTransactions (POST) Resource debits (pulls) funds from a sender's Visa account (in preparation for pushing funds to a recipient's account)
+// by initiating a financial message called an Account Funding Transaction (AFT)
+func PullFundsTransactionsPost(request PullFundsTransactionRequest) (response PullFundsTransactionResponse, err error) {
+	/*
+	   You should log or otherwise retain all the information returned in the PullFundsTransactions response.
+	   Should it be necessary to initiate a ReverseFundsTransactions POST operation, you may need to provide
+	   several of the PullFundsTransactions Response values in the Request.
+	*/
+	body, err := json.Marshal(request)
+	if err != nil {
+		return response, err
+	}
+	responseJson, err := Client(USER_ID, USER_PASSWORD, PULL_FUNDS_TRANSACTIONS_URL, "POST", false, body, "0")
+	if err != nil {
+		return response, err
+	}
+	// Unmarshall response
+	err = json.Unmarshal(responseJson, &response)
+	if err != nil {
+		return response, err
+	}
+	return
+}
+
+func Client(userId string, userPassword string, url string, reqType string, production bool, body []byte, transactionID string) (response []byte, err error) {
+	authHeader := createAuthHeader()
+
+	//fmt.Printf("Body: %v\n", bytes.NewBuffer(body))
+	req, err := http.NewRequest(reqType, url, bytes.NewBuffer(body))
+	req.Header.Set("X-Client-Transaction-ID", transactionID)
+	req.Header.Set("Authorization:Basic ", authHeader)
+	req.Header.Set("Content-Type", "application/json")
+	//req.Header.Set("Accept", "application/json,application/octet-stream")
+	req.Header.Set("Accept", "application/json")
+
+	// Load client cert
+	cert, err := tls.LoadX509KeyPair(SSL_PUBLIC_KEY_PATH, SSL_PRIVATE_KEY_PATH)
+	if err != nil {
+		return nil, fmt.Errorf("Could not load key pair: %v", err)
+	}
+	// Load CA cert
+	caCert, err := ioutil.ReadFile(SSL_CAPRIVATE_KEY_PATH)
+	if err != nil {
+		return nil, fmt.Errorf("Could not load CA key: %v", err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	// Setup HTTPS client
+	tlsConfig := &tls.Config{
+		Certificates:       []tls.Certificate{cert},
+		RootCAs:            caCertPool,
+		InsecureSkipVerify: true, //@FIXME: This call *must* be secure
+	}
+	tlsConfig.BuildNameToCertificate()
+	transport := &http.Transport{TLSClientConfig: tlsConfig}
+	client := &http.Client{Transport: transport}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("Could not load HTTPS client: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		response, _ = ioutil.ReadAll(resp.Body)
+		return nil, fmt.Errorf("HTTP error: %s, %v", resp.Status, string(response))
+	}
+
+	response, _ = ioutil.ReadAll(resp.Body)
+	fmt.Printf("Response: %v\n", string(response))
+	return response, nil
+}
+
+func createAuthHeader() (authHeader string) {
+	// Auth header = \ase64(userid:user_password)
+	authHeader = base64.StdEncoding.EncodeToString([]byte(USER_ID + ":" + USER_PASSWORD))
+	return
+}
---
+++ SampleCode/VisaAPICalls/Go/VisaDirectSample_test.go
@@ -0,0 +1,157 @@
+package visasample
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestPullFundsTransactionPost(t *testing.T) {
+	cases := []struct {
+		systemsTraceAuditNumber        int
+		retrievalReferenceNumber       string
+		localTransactionDateTime       string
+		acquiringBin                   int
+		acquirerCountryCode            int
+		senderPrimaryAccountNumber     string
+		senderCardExpiryDate           string
+		senderCurrencyCode             string
+		amount                         float64
+		surcharge                      float64
+		cavv                           string
+		foreignExchangeFeeTransaction  float64
+		businessApplicationId          string
+		merchantCategoryCode           int
+		CAname                         string
+		CAterminalId                   string
+		CAidCode                       string
+		CAAstate                       string
+		CAAcounty                      string
+		CAAcountry                     string
+		CAAzipCode                     string
+		MSDtrack1Data                  string
+		MSDtrack2Data                  string
+		POSDpanEntryMode               int
+		POSDposConditionCode           int
+		POSDmotoECIIndicator           string
+		POSCposTerminalType            int
+		POSCposTerminalEntryCapability int
+		PDpinDataBlock                 string
+		PDSRCIpinBlockFormatCode       int
+		PDSRCIzoneKeyIndex             int
+		feeProgramIndicator            string
+	}{
+		//{},
+		{
+			123456,
+			"407509300259",
+			time.Now().Format("2006-01-02T03:04:05"),
+			409999,
+			101,
+			"4957030100009952",
+			"2020-03",
+			"USD",
+			110.,
+			2.00,
+			"0000010926000071934977253000000000000000",
+			10.00,
+			"AA",
+			6012,
+			"Saranya",
+			"365539",
+			"VMT200911026070",
+			"CA",
+			"081",
+			"USA",
+			"94404",
+			"", //"1010101010101010101010101010",
+			"",
+			90,
+			0,
+			"0",
+			4,
+			2,
+			"",
+			0,
+			0,
+			"123",
+		},
+	}
+
+	for _, c := range cases {
+		cardAcceptorAddress := CardAcceptorAddress{
+			State:   c.CAAstate,
+			County:  c.CAAcounty,
+			Country: c.CAAcountry,
+			ZipCode: c.CAAzipCode,
+		}
+		cardAcceptor := CardAcceptor{
+			Name:       c.CAname,
+			TerminalId: c.CAterminalId,
+			IdCode:     c.CAidCode,
+			Address:    cardAcceptorAddress,
+		}
+		/*magneticStripeData := MagneticStripeData{
+			Track1Data: c.MSDtrack1Data,
+			Track2Data: c.MSDtrack2Data,
+		}*/
+		/*
+			pointOfServiceData := PointOfServiceData{
+				PanEntryMode:     c.POSDpanEntryMode,
+				PosConditionCode: c.POSDposConditionCode,
+				MotoECIIndicator: c.POSDmotoECIIndicator,
+			}
+		*/
+		/*
+			pointOfServiceCapability := PointOfServiceCapability{
+				PosTerminalType:            c.POSCposTerminalType,
+				PosTerminalEntryCapability: c.POSCposTerminalEntryCapability,
+			}
+		*/
+		/*
+			securityRelatedControlInfo := SecurityRelatedControlInfo{
+				PinBlockFormatCode: c.PDSRCIpinBlockFormatCode,
+				ZoneKeyIndex:       c.PDSRCIzoneKeyIndex,
+			}
+		*/
+		/*
+			pinData := PinData{
+				PinDataBlock: c.PDpinDataBlock,
+				//SecurityRelatedControlInfo: securityRelatedControlInfo,
+			}
+		*/
+		request := PullFundsTransactionRequest{
+			SystemsTraceAuditNumber:    c.systemsTraceAuditNumber,
+			RetrievalReferenceNumber:   c.retrievalReferenceNumber,
+			LocalTransactionDateTime:   c.localTransactionDateTime,
+			AcquiringBin:               c.acquiringBin,
+			AcquirerCountryCode:        c.acquirerCountryCode,
+			SenderPrimaryAccountNumber: c.senderPrimaryAccountNumber,
+			SenderCardExpiryDate:       c.senderCardExpiryDate,
+			SenderCurrencyCode:         c.senderCurrencyCode,
+			Amount:                     c.amount,
+			Surcharge:                  c.surcharge,
+			Cavv:                       c.cavv,
+			ForeignExchangeFeeTransaction: c.foreignExchangeFeeTransaction,
+			BusinessApplicationId:         c.businessApplicationId,
+			MerchantCategoryCode:          c.merchantCategoryCode,
+			CardAcceptor:                  cardAcceptor,
+			//MagneticStripeData:            &magneticStripeData,
+			//PointOfServiceData:       &pointOfServiceData,
+			//PointOfServiceCapability: &pointOfServiceCapability,
+			//PinData:                  &pinData,
+			FeeProgramIndicator: c.feeProgramIndicator,
+		}
+
+		response, err := PullFundsTransactionsPost(request)
+		if err != nil {
+			t.Errorf("Error when getting response: %v", err)
+		}
+		fmt.Printf("%+v\n", response)
+		// 1. Check type
+		if reflect.TypeOf(response).String() != "visa.PullFundsTransactionResponse" {
+			t.Errorf("Return should be of type PullFundsTransactionResponse. Looking for %s, got %s", "visa.PullFundsTransactionResponse", reflect.TypeOf(response).String())
+		}
+	}
+}
